978c99e5f9c277a2bbe83dd67e3b000ba53e7d3e
0xantman <antho74830@gmail.com>
2021-02-02 01:43:30 +0100

    Add gitignore and rm unused

--- contract/Crowdfunding.sol
+++ contract/Crowdfunding.sol
@@ -9,13 +9,12 @@ contract Init {
     
     function proposal(string memory _description) 
     view public returns (uint8) {
-        msg.sender;
         Proposal memory myStruct = Proposal({description: _description, owner: msg.sender});
         uint8 descriptionLength = uint8(bytes(myStruct.description).length);
-        
         if (bytes(myStruct.description).length > 255) {
             //require("player doesn't exist");
             revert("error length should be lower than 255");
+
         } else {
             return descriptionLength;
         }
