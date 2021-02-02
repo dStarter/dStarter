60cb3114550f4ea3ad6324db3fc8e38b0d8b2e3d
Dams2 <damien-rojo@hotmail.fr>
2021-02-02 01:20:32 +0100

    First commit

---
+++ contract/Crowdfunding.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.7.0 <0.8.0;
+
+contract Init {
+    
+    struct Proposal {
+        string description;
+        address owner;
+    }
+    
+    function proposal(string memory _description) 
+    view public returns (uint8) {
+        msg.sender;
+        Proposal memory myStruct = Proposal({description: _description, owner: msg.sender});
+        uint8 descriptionLength = uint8(bytes(myStruct.description).length);
+        
+        if (bytes(myStruct.description).length > 255) {
+            //require("player doesn't exist");
+            revert("error length should be lower than 255");
+        } else {
+            return descriptionLength;
+        }
+    }
+}
