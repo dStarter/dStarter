e28143c6b156102d3976f6c20fda4a767d396088
0xantman <antho74830@gmail.com>
2021-02-03 15:12:43 +0100

    Prepare dev

--- contract/Crowdfunding.sol
+++ contract/Crowdfunding.sol
@@ -1,5 +1,10 @@
+// SPDX-License-Identifier: agpl-3.0
 pragma solidity >=0.7.0 <0.8.0;
-
+/*
+ * @title Proposal
+ * @dev Implement crowdfunding proposal
+ * @author dStart
+ */
 contract Init {
     
     struct Proposal {
@@ -11,10 +16,9 @@ contract Init {
     view public returns (uint8) {
         Proposal memory myStruct = Proposal({description: _description, owner: msg.sender});
         uint8 descriptionLength = uint8(bytes(myStruct.description).length);
-        if (bytes(myStruct.description).length > 255) {
-            //require("player doesn't exist");
-            revert("error length should be lower than 255");
 
+        if (bytes(myStruct.description).length > 255) {
+            revert("error length should be lower than 255");
         } else {
             return descriptionLength;
         }
