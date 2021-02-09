4cf4d11d1b0f7ae92215c958b986a16ed864bbb6
Dams2 <damien-rojo@hotmail.fr>
2021-02-09 18:59:05 +0100

    ADD modifiers

--- contract/SimpleProposal.sol
+++ contract/SimpleProposal.sol
@@ -10,8 +10,6 @@ pragma abicoder v2;
 //import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract SimpleProposal {
-
-
     address constant dStarterToken = 0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47;
     StatusInfos constant STATUS_DEFAULT = StatusInfos.inProgress;
     uint proposalTotalNumber = 0;
@@ -47,7 +45,7 @@ contract SimpleProposal {
         uint _blockNumberEnd,
         uint _goalAmount,
         bool _privateSale
-    ) public returns (uint){
+    ) public proposalRequirement(_description, _blockNumberEnd, _goalAmount) returns (uint){
         uint proposalNumber = proposalTotalNumber;
         uint emptyTotalHarvest = 0;
         address[] memory emptyInvestorAddressList;
@@ -64,18 +62,7 @@ contract SimpleProposal {
         privateSaleAddressList: emptyPrivateAddressList,
         status: STATUS_DEFAULT
         });
-        require(
-            bytes(_proposal.description).length <= 255,
-            "Error - The length of the description must be less than 255 characters"
-        );
-        require(
-            _blockNumberEnd > block.number,
-            "Error - blockNumberEnd must be greater than blockNumberStart"
-        );
-        require(
-            _goalAmount > 0,
-            "Error - goalAmount must be greater than 0"
-        );
+
         simpleList[proposalNumber] = _proposal;
         allProposalsNumbers[proposalNumber] = proposalNumber;
         proposalTotalNumber++;
@@ -85,44 +72,23 @@ contract SimpleProposal {
     function addPrivateSaleAddress(
         uint proposalNumber,
         address[] memory privateSaleAddressList
-    ) public {
-        require(
-            simpleList[proposalNumber].owner == msg.sender,
-            "Error - not autorized "
-        );
-        require(
-            simpleList[proposalNumber].privateSale == true,
-            "Error - this proposal is not private"
-        );
+    ) public
+    onlyOwner(proposalNumber)
+    onlyPrivateSale(proposalNumber) {
         simpleList[proposalNumber].privateSaleAddressList = privateSaleAddressList;
     }
 
     function creatorRejectsTheProposal(
         uint proposalNumber
-    ) public {
-        require(
-            simpleList[proposalNumber].owner == msg.sender,
-            "Error - not autorized "
-        );
+    ) public onlyOwner(proposalNumber){
         simpleList[proposalNumber].status = StatusInfos.rejectedByCreator;
     }
 
     function invest(
         uint proposalNumber
-    ) public payable returns (Investment memory){
-        require(allProposalsNumbers[proposalNumber] == proposalNumber, "Error - this proposal doesn't exist");
-
-        if (simpleList[proposalNumber].blockNumberEnd < block.number){
-            if (simpleList[proposalNumber].totalHarvest < simpleList[proposalNumber].goalAmount) {
-                simpleList[proposalNumber].status = StatusInfos.rejected;
-            } else {
-                simpleList[proposalNumber].status = StatusInfos.ended;
-            }
-        }
-
-        require(simpleList[proposalNumber].status == StatusInfos.inProgress, "Error - this proposal is ended");
+    ) public payable proposalExistAndIfItsStatusIsInProgress(proposalNumber)
+    returns (Investment memory){
         require(msg.value > 0, "Error - send some value to invest in this proposal");
-
         simpleList[proposalNumber].investorAddressList.push(msg.sender);
         simpleList[proposalNumber].totalHarvest += msg.value;
 
@@ -139,15 +105,38 @@ contract SimpleProposal {
         return investmentList[msg.sender][proposalNumber];
     }
 
-    function investorGetRefund(uint proposalNumber, uint amount) public {
-        checkIfProposalExistAndIfItsStatusIsInProgress(proposalNumber);
+    function investorGetRefund(uint proposalNumber, uint amount) public proposalExistAndIfItsStatusIsInProgress(proposalNumber) {
+
     }
 
-    // Helper
+    // - Modifiers:
 
-    function checkIfProposalExistAndIfItsStatusIsInProgress(uint proposalNumber) private {
+    modifier proposalRequirement(
+        string memory _description,
+        uint _blockNumberEnd,
+        uint _goalAmount
+    ){
+        require(bytes(_description).length <= 255, "Error - The length of the description must be less than 255 characters");
+        _;
+        require(_blockNumberEnd > block.number, "Error - blockNumberEnd must be greater than block number");
+        _;
+        require(_goalAmount > 0, "Error - goalAmount must be greater than 0");
+        _;
+    }
+
+    modifier onlyOwner(uint proposalNumber) {
+        require(simpleList[proposalNumber].owner == msg.sender, "Error - not autorized ");
+        _;
+    }
+
+    modifier onlyPrivateSale(uint proposalNumber) {
+        require(simpleList[proposalNumber].privateSale == true, "Error - this proposal is not private");
+        _;
+    }
+
+    modifier proposalExistAndIfItsStatusIsInProgress(uint proposalNumber) {
         require(allProposalsNumbers[proposalNumber] == proposalNumber, "Error - this proposal doesn't exist");
-
+        _;
         if (simpleList[proposalNumber].blockNumberEnd < block.number){
             if (simpleList[proposalNumber].totalHarvest < simpleList[proposalNumber].goalAmount) {
                 simpleList[proposalNumber].status = StatusInfos.rejected;
@@ -155,7 +144,7 @@ contract SimpleProposal {
                 simpleList[proposalNumber].status = StatusInfos.ended;
             }
         }
-
         require(simpleList[proposalNumber].status == StatusInfos.inProgress, "Error - this proposal is ended");
+        _;
     }
 }
